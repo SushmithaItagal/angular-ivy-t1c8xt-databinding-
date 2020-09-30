c063d6d77aad7dea6dc24494ae25baa162a6fed7
SushmithaItagal <irsushmitha15@gmail.com>
2020-10-01 00:54:17 +0530

    3 files modified

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector:'app-server',
-  templateUrl:'./server.component.html'
+  templateUrl:'./server.component.html',
   styles:[`
   online{
     color : white;
+    }
     `]
   
 })
